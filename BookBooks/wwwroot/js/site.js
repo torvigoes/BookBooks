a30a0c8d034b4bc09356524d0e11700ebeb5a8c7
Vitor Góes <vitornevesgoes@gmail.com>
2025-08-12 20:16:07 -0300

    added jquery datatable

--- BookBooks/wwwroot/js/site.js
+++ BookBooks/wwwroot/js/site.js
@@ -1,25 +1,53 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-$('.close-alert').click(function () {
-    $('.alert').hide('hide');
-})
-
+// O $(function() { ... }); é a forma curta e moderna de $(document).ready(function() { ... });
 $(function () {
-    $('.open-delete-modal').on('click', function () {
+    $('#table-livros').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": ">",
+                "sPrevious": "<",
+                "sFirst": "<<",
+                "sLast": ">>"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+
+    $('.close-alert').click(function () {
+        $('.alert').hide('hide');
+    });
+
+    $(document).on('click', '.open-delete-modal', function () {
         var url = $(this).data('url');
 
         $('#modal-placeholder').load(url, function () {
-
             var modalElement = $('#modal-placeholder').find('.modal');
             var modalInstance = new bootstrap.Modal(modalElement[0]);
             modalInstance.show();
 
-            // Opcional: limpa o placeholder quando o modal é fechado
             modalElement.on('hidden.bs.modal', function () {
                 $('#modal-placeholder').empty();
             });
         });
     });
+
 });
